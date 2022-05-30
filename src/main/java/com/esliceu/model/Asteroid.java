3b922a0846f701d886f6ce0e0ef7661559025209
Your Name <you@example.com>
2022-05-30 16:02:50 +0200

    getters & setters

--- src/main/java/com/esliceu/model/Asteroid.java
+++ src/main/java/com/esliceu/model/Asteroid.java
@@ -31,4 +31,164 @@ public class Asteroid {
         this.distancia = distancia;
         this.diametre = diametre;
     }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNomCientific() {
+        return nomCientific;
+    }
+
+    public void setNomCientific(String nomCientific) {
+        this.nomCientific = nomCientific;
+    }
+
+    public Double getVelocitat() {
+        return velocitat;
+    }
+
+    public void setVelocitat(Double velocitat) {
+        this.velocitat = velocitat;
+    }
+
+    public Double getVelocitatMinima() {
+        return velocitatMinima;
+    }
+
+    public void setVelocitatMinima(Double velocitatMinima) {
+        this.velocitatMinima = velocitatMinima;
+    }
+
+    public Double getVelocitatMaxima() {
+        return velocitatMaxima;
+    }
+
+    public void setVelocitatMaxima(Double velocitatMaxima) {
+        this.velocitatMaxima = velocitatMaxima;
+    }
+
+    public Boolean getEsPerillos() {
+        return esPerillos;
+    }
+
+    public void setEsPerillos(Boolean esPerillos) {
+        this.esPerillos = esPerillos;
+    }
+
+    public int getIndexPerillositat() {
+        return indexPerillositat;
+    }
+
+    public void setIndexPerillositat(int indexPerillositat) {
+        this.indexPerillositat = indexPerillositat;
+    }
+
+    public Double getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(Double distancia) {
+        this.distancia = distancia;
+    }
+
+    public Double getDistanciaMinima() {
+        return distanciaMinima;
+    }
+
+    public void setDistanciaMinima(Double distanciaMinima) {
+        this.distanciaMinima = distanciaMinima;
+    }
+
+    public Double getDistanciaMaxima() {
+        return distanciaMaxima;
+    }
+
+    public void setDistanciaMaxima(Double distanciaMaxima) {
+        this.distanciaMaxima = distanciaMaxima;
+    }
+
+    public Double getDiametre() {
+        return diametre;
+    }
+
+    public void setDiametre(Double diametre) {
+        this.diametre = diametre;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public Double getAmplada() {
+        return amplada;
+    }
+
+    public void setAmplada(Double amplada) {
+        this.amplada = amplada;
+    }
+
+    public Double getProfunditat() {
+        return profunditat;
+    }
+
+    public void setProfunditat(Double profunditat) {
+        this.profunditat = profunditat;
+    }
+
+    public String[] getComposicio() {
+        return composicio;
+    }
+
+    public void setComposicio(String[] composicio) {
+        this.composicio = composicio;
+    }
+
+    public Double getRadi() {
+        return radi;
+    }
+
+    public void setRadi(Double radi) {
+        this.radi = radi;
+    }
+
+    public Double getOrbita() {
+        return orbita;
+    }
+
+    public void setOrbita(Double orbita) {
+        this.orbita = orbita;
+    }
+
+    public String[] getPlanetesPropers() {
+        return planetesPropers;
+    }
+
+    public void setPlanetesPropers(String[] planetesPropers) {
+        this.planetesPropers = planetesPropers;
+    }
+
+    public Date getPrimeraVisualitzacio() {
+        return primeraVisualitzacio;
+    }
+
+    public void setPrimeraVisualitzacio(Date primeraVisualitzacio) {
+        this.primeraVisualitzacio = primeraVisualitzacio;
+    }
+
+    public Date getDiaMesProximALaTerra() {
+        return diaMesProximALaTerra;
+    }
+
+    public void setDiaMesProximALaTerra(Date diaMesProximALaTerra) {
+        this.diaMesProximALaTerra = diaMesProximALaTerra;
+    }
 }
